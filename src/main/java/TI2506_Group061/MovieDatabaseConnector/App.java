1eed2a3411817b331f1586905edc41e8cd3e789a
alangerak <aaclangerak@gmail.com>
2017-01-07 23:11:49 +0100

    Working example of DAO

--- src/main/java/TI2506_Group061/MovieDatabaseConnector/App.java
+++ src/main/java/TI2506_Group061/MovieDatabaseConnector/App.java
@@ -1,48 +1,19 @@
 package TI2506_Group061.MovieDatabaseConnector;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.time.Year;
 import vanilla.Movie;
+import vanilla.MovieDAO;
+import vanilla.MovieDAO_Postgres;
 
 public class App {
 
 	public static void main(String[] argv) {
 
-		DAOManager manager = DAOManager.getInstance();
-		Connection connection = manager.getDBConnection();
-
-		if (connection != null) {
-			try {
-				viewTable(connection);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static void viewTable(Connection con) throws SQLException {
-
-		String selectTableSQL = "SELECT * FROM movies LIMIT 100";
-		Statement statement = con.createStatement();
-		ResultSet rs = statement.executeQuery(selectTableSQL);
-		System.out.println();
-		System.out.format("%10s%60s%15s%15s%20s%20s%20s", "ID", "Title", "Year", " Number", "Type","Location","Language \n");
+		MovieDAO movietest = new MovieDAO_Postgres();
+		Movie resultMovie = movietest.getMovie("Blade");
 		
-		while (rs.next()) {
-			Movie movie = new Movie();
-			movie.setId(rs.getInt("idmovies"));
-			movie.setTitle(rs.getString("title"));
-			movie.setYear(Year.of(rs.getInt("year")));
-			movie.setType(rs.getString("type"));
-			movie.setLocation(rs.getString("location"));
-			movie.setLanguage(rs.getString("language"));
-
-			movie.printMovieObject();
-
-		}
-
+		System.out.format("%10s%60s%15s%15s%20s%20s%20s", "ID", "Title", "Year", " Number", "Type","Location","Language \n");
+		resultMovie.printMovieObject();
+		
 	}
+
 }
