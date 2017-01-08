ab9eea91176e0b13b34de1911ed39c7897096c36
alangerak <aaclangerak@gmail.com>
2017-01-08 12:30:39 +0100

    fix error startup server

--- startServer.bat
+++ startServer.bat
@@ -5,3 +5,9 @@ SET postgres_db_data_dir="data"
 
 cd /d %postgres_install_dir%
 bin\postgres.exe -D %postgres_db_data_dir%
+
+REM Sleep because postmaster process has to be restarted or something
+TIMEOUT 60
+
+cd /d %postgres_install_dir%
+bin\postgres.exe -D %postgres_db_data_dir%
